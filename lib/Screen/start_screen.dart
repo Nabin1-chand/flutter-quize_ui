2aa9e1f0b4b12b9cac182270eec7ed4e6e0aba9a
Nabin12chand <nabin.async.chand@gmail.com>
2023-12-18 22:59:02 +0545

    refactor the screen

--- lib/Screen/start_screen.dart
+++ lib/Screen/start_screen.dart
@@ -18,10 +18,11 @@ class _StartScreenState extends State<StartScreen> {
         const SizedBox(
           height: 30,
         ),
-        OutlinedButton(
+        OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            child: const Text('Start Quiz'))
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'))
       ],
     ));
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quize_app/Screen/start_screen.dart';
+import 'package:quize_app/Screen/quize_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 78, 13, 151),
-            Color.fromARGB(255, 107, 15, 168),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomLeft,
-        )),
-        child: const StartScreen(),
-      ),
-    ),
-  ));
+  runApp(const Quiz());
 }
